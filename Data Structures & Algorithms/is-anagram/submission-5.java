5d2d73a2254373d077df9ea137d46a5c5077152b
Chennuriharish <42451372+Chennuriharish@users.noreply.github.com>
2026-08-19 09:45:14 -0500

    Add: is-anagram - submission-5

---
+++ Data Structures & Algorithms/is-anagram/submission-5.java
@@ -0,0 +1,44 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+
+        if (s == null && t == null) {
+            return true;
+        }
+
+        if( s == null || t == null) {
+            return false;
+        }
+
+        Map<Character, Integer> sMap = new HashMap<>();
+        Map<Character, Integer> tMap = new HashMap<>();
+
+        for(int i=0; i< s.length(); i++) {
+            if(!sMap.containsKey(s.charAt(i))) {
+                sMap.put(s.charAt(i), 1);
+            } else {
+                sMap.put(s.charAt(i), sMap.get(s.charAt(i))+1);
+            }
+        }
+
+        for(int i=0; i< t.length(); i++) {
+            if(!tMap.containsKey(t.charAt(i))) {
+                tMap.put(t.charAt(i), 1);
+            } else {
+                tMap.put(t.charAt(i), tMap.get(t.charAt(i))+1);
+            }
+        }
+
+        if(sMap.size() != tMap.size()) {
+            return false;
+        }
+
+        for(char c: sMap.keySet()) {
+            if(!sMap.get(c).equals(tMap.get(c))) {
+                return false;
+            }
+        }
+
+        return true;
+
+    }
+}
